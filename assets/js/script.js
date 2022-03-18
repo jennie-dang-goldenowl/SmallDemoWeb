0c938816623d15a0f064caee6632da03fb40de04
Dang Le Thien Thanh <jennie.dang.goldenowl@gmail.com>
2022-03-18 15:34:12 +0700

    fix validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,71 +3,10 @@
     $(document).ready(function () {
     $("#search-box").on("keyup", function (event) {
         event.preventDefault();
-        /* Act on the event */
         var keyWord = $(this).val().toLowerCase();
         $(".table-hover tr").filter(function () {
         $(this).toggle($(this).text().toLowerCase().indexOf(keyWord) > -1);
         });
-    });
-    });
-
-    //show
-    function myFunction() {
-    $(".filter").toggle();
-    }
-
-//filter
-    $().ready(function () {
-    $("#formCreatePro").validate({
-        onfocusout: false,
-        onkeyup: false,
-        onclick: false,
-        rules: {
-        idPro: {
-            required: true,
-        },
-        name: {
-            required: true,
-        },
-        dev: {
-            required: true,
-        },
-        idDev: {
-            required: true,
-        },
-        fName: {
-            required: true,
-        },
-        lName: {
-            required: true,
-        },
-        pro: {
-            required: true,
-        },
-        },
-        messages: {
-        idPro: {
-            required: "Please enter ID Project",
-        },
-        name: {
-            required: "Please enter name project",
-        },
-        dev: {
-            required: "Please choose our dev",
-        },
-        idDev: {
-            required: "Please enter ID Project",
-        },
-        lname: {
-            required: "Please enter name project",
-        },
-        pro: {
-            required: "Please choose our dev",
-        },
-        },
-        submitHandler: function (form) {
-        form.create();
-        },
     });
     });
 
@@ -91,7 +30,7 @@ function filterDay() {
         tdStart = tr[i].getElementsByTagName("td")[3];
         tdEnd = tr[i].getElementsByTagName("td")[4];
         if (tdStart && tdEnd) {
-        if ((tdStart >= inputStartDate) && (tdEnd <= inputEndDate)) {
+        if ((tdStart > inputStartDate) && (tdEnd < inputEndDate)) {
         table.style.display = "table";
         tr[i].style.display = "";
         } else {
@@ -101,5 +40,4 @@ function filterDay() {
     }
     }
 }
-//gán sự kiện cho thẻ input
-input.addEventListener("keyup", myFunction);
+inputEndDate.addEventListener("keyup", filterDay);
