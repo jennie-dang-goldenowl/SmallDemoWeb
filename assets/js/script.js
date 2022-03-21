3089ea14e9548aceb144c3bf05633c70bd1478bb
Dang Le Thien Thanh <jennie.dang.goldenowl@gmail.com>
2022-03-21 07:47:41 +0700

    done filter

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,20 +26,31 @@ function searchFunction() {
 //filter
 var inputStartDate = document.getElementById("inputStartDate");
 var inputEndDate = document.getElementById("inputEndDate");
-var timeInputStart = parseDate(inputStartDate).getTime();
-var timeInputEnd = parseDate(inputEndDate).getTime();
+// var timeInputStart = inputStartDate.getTime();
+// var timeInputEnd = inputEndDate.getTime();
 
 function filterDay() {
-  var table, tr, tdStart, tdEnd, i, timeStart, timeEnd;
+  var table, tr, tdStart, tdEnd, i, timeStart, timeEnd, yearS, monthS, dayS, yearE, monthE, dayE;
   table = document.getElementById("example");
   tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; i++) {
+  for (i = 1; i < tr.length; i++) {
+    debugger
     tdStart = tr[i].getElementsByTagName("td")[3];
-    timeStart = parseDate(tdStart).getTime();
+    tdStart = tdStart.textContent;
+    timeStart = tdStart.split("/").reverse().join("");
+    yearS = +timeStart.substring(0, 4);
+    monthS = +timeStart.substring(4, 6);
+    dayS = +timeStart.substring(6, 8);
+    timeStart = new Date(yearS, monthS - 1, dayS);
     tdEnd = tr[i].getElementsByTagName("td")[4];
-    timeEnd = parseDate(tdEnd).getTime();
-    if (startTime > inputStartDate && endTime < inputEndDate) {
-      tr[i].style.display = "";
+    tdEnd = tdEnd.textContent;
+    timeEnd = tdEnd.split("/").reverse().join("");
+    yearE = +timeEnd.substring(0, 4);
+    monthE = +timeEnd.substring(4, 6);
+    dayE = +timeEnd.substring(6, 8);
+    timeEnd = new Date(yearE, monthE - 1, dayE);
+    if (timeStart >= inputStartDate && timeEnd <= inputEndDate) {
+      tr[i].style.display = "block";
     } else {
       tr[i].style.display = "none";
     }
