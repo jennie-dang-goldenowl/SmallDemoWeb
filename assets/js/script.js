4dcecb8e6d47251cc93076929d585e68f3cfb63a
Dang Le Thien Thanh <jennie.dang.goldenowl@gmail.com>
2022-03-21 10:04:24 +0700

    doen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 //pagination
 $(document).ready(function () {
-  $("#example").DataTable();
+  $("#tableProject").DataTable();
 });
 
 function searchFunction() {
@@ -8,7 +8,7 @@ function searchFunction() {
   var input, filter, table, tr, td, i, txtValue;
   input = document.getElementById("search-box");
   filter = input.value.toUpperCase();
-  table = document.getElementById("example");
+  table = document.getElementById("tableProject");
   tr = table.getElementsByTagName("tr");
   for (i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td")[1];
@@ -24,31 +24,22 @@ function searchFunction() {
 }
 
 //filter
-var inputStartDate = document.getElementById("inputStartDate");
-var inputEndDate = document.getElementById("inputEndDate");
-// var timeInputStart = inputStartDate.getTime();
-// var timeInputEnd = inputEndDate.getTime();
+let inputStartDate = document.getElementById("inputStartDate").textContent;
+let inputEndDate = document.getElementById("inputEndDate").textContent;
 
 function filterDay() {
-  var table, tr, tdStart, tdEnd, i, timeStart, timeEnd, yearS, monthS, dayS, yearE, monthE, dayE;
-  table = document.getElementById("example");
-  tr = table.getElementsByTagName("tr");
+  var tdStart, tdEnd, i, timeStart, timeEnd;
+  let table = document.getElementById("tableProject");
+  let tr = table.getElementsByTagName("tr");
   for (i = 1; i < tr.length; i++) {
-    debugger
     tdStart = tr[i].getElementsByTagName("td")[3];
     tdStart = tdStart.textContent;
-    timeStart = tdStart.split("/").reverse().join("");
-    yearS = +timeStart.substring(0, 4);
-    monthS = +timeStart.substring(4, 6);
-    dayS = +timeStart.substring(6, 8);
-    timeStart = new Date(yearS, monthS - 1, dayS);
+    timeStart = tdStart.split("/");
+    timeStartDate = new Date(timeStart[2], timeStart[1] - 1, timeStart[0]);
     tdEnd = tr[i].getElementsByTagName("td")[4];
     tdEnd = tdEnd.textContent;
-    timeEnd = tdEnd.split("/").reverse().join("");
-    yearE = +timeEnd.substring(0, 4);
-    monthE = +timeEnd.substring(4, 6);
-    dayE = +timeEnd.substring(6, 8);
-    timeEnd = new Date(yearE, monthE - 1, dayE);
+    timeEnd = tdEnd.split("/");
+    timeEndDate = new Date(timeEnd[2], timeEnd[1] - 1, timeEnd[0]);
     if (timeStart >= inputStartDate && timeEnd <= inputEndDate) {
       tr[i].style.display = "block";
     } else {
